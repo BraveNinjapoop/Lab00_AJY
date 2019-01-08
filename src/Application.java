55a459b60ec1932581b34bccf6e2466dfb69552c
Austin Young <ajyoung0123@gmail.com>
2019-01-08 10:26:57 -0500

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Circle c = new Circle ();
+		
+		System.out.printf("%.2f%n", c.getRadius()); //The printf is restriciting the circle from generating
+													//anything more than two numbers/decimals
+		
+	}//end main
+
+}//end class
